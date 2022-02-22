6c36837dd29c61deb30265a7438f82ca93cfca59
Marcos Rodriguez-Lapido <91735876+MarcRodlap@users.noreply.github.com>
2022-02-22 02:57:56 +0000

    .

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 from replit import db
 
-# Board is 8x8
+from art import tprint
+import os
+tprint("H O P C h e s s", "Boxing")
+print("--by Lucas Garcia, Marcos Rodriguez-Lapido, Emilio Dibildox--\n\n")
+
+
+import random
+
+#grabs players name and randomly gives them dark or light pieces
+darkPlayer = []
+lightPlayer = []
+select = []
+def NameSelection():
+  
+  playerOne = input("input player 1 name: ")
+  select.append(playerOne)
+  playerTwo = input("input player 2 name: ")
+  select.append(playerTwo)
+  
+  select.remove(random.choice(select))
+  darkPlayer.append(select[0])
+  
+  if playerOne in darkPlayer:
+    lightPlayer.append(playerTwo)
+    print(playerOne + " is playing the dark pieces and going first")
+    print(playerTwo + " is playing thelight pieces and is going second")
+  else:
+    lightPlayer.append(playerOne)
+    print(playerTwo + " is playing the dark pieces and going first")
+    print(playerOne + " is playing thelight pieces and is going second")
+    
+ 
+
+  
+  
+
+# creates Board that is 8x8 
 def createBoard():
     dims=(8, 8)
     board = []
@@ -31,19 +67,8 @@ def assignRest(board, bw):
   
 # From is a tuple of (x, y)
 # To is a tuple of (x, y)
-  
-def movePiece(board, initial, final):
-    Ls = ["♖", "♘", "♗", "♕", "♔","♙"]
-    
-    piece = board[initial[0]][initial[1]]
-    board[initial[0]][initial[1]] = " "
-    
-    if board[final[0]][final[1]] in Ls:
-      board[final[0]][final[1]] = " "
-    
-    board[final[0]][final[1]] = piece    
-    return board
 
+#the move piece function moves the piece to the players inputed location while also checking if it landed on an enemy piece to eat
 def movePiece(board, initial, final):
     LsW = ["♖", "♘", "♗", "♕", "♔","♙"]
     LsB = ["♜", "♞", "♝", "♛", "♚","♟"]
@@ -67,6 +92,7 @@ def movePiece(board, initial, final):
       board[final[0]][final[1]] = piece    
       return board
 
+#if you select to move a pawn this makes sure you only move what a pawn is capable of 
 def movePawn(board, bw, initial, final):
     legalMove = None
     if bw=="White":
@@ -79,6 +105,7 @@ def movePawn(board, bw, initial, final):
         print("Illegal Move")
         return board
 
+#if you select to move a knight this makes sure you only move what a knight is capable of 
 def moveKnight(board, bw, initial, final):
   legalMove = [[1,2],[2,1],[-2,1],[2,-1],[-2,-1],[-1,-2],[-1,2],[1,-2]]
   w = len(legalMove)
@@ -98,10 +125,7 @@ def moveKnight(board, bw, initial, final):
       print("Illegal Move")
       return board
    
-    
-
-  
-
+#if you select to move a bishop this makes sure you only move what a bishop is capable of 
 def moveBishop(board, bw, initial, final):
   legalMove = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7],[-1,1],[-2,2],[-3,3],[-4,4],[-5,5],[-6,6],[-7,7],[1,-1],[2,-2],[3,-3],[4,-4],[5,-5],[6,-6],[7,-7]]
   w = len(legalMove)
@@ -121,6 +145,7 @@ def moveBishop(board, bw, initial, final):
       print("Illegal Move")
       return board
 
+#if you select to move a rook this makes sure you only move what a rook is capable of
 def moveRook(board, bw, initial, final):
   if initial[0] == final[0]:
     if initial[1] != final[1]:
@@ -137,9 +162,9 @@ def moveRook(board, bw, initial, final):
       return board
   
   
-
+#if you select to move a king this makes sure you only move what a king is capable of 
 def moveKing(board, bw, initial, final):
-  legalMove = [[1,2],[2,1],[-2,1],[2,-1],[-2,-1],[-1,-2],[-1,2],[1,-2]]
+  legalMove = [[1,1],[1,-1],[-1,0],[0,-1],[0,1],[1,0],[-1,-1],[-1,1]]
   w = len(legalMove)
   for i in range(len(legalMove)):
     print ("~")
@@ -157,7 +182,7 @@ def moveKing(board, bw, initial, final):
       print("Illegal Move")
       return board
 
-
+#if you select to move a Queen this makes sure you only move what a Queen is capable of(combined bishop & rook checks)
 def moveQueen(board, bw, initial, final):
   if initial[0] == final[0]:
     if initial[1] != final[1]:
@@ -192,86 +217,123 @@ def moveQueen(board, bw, initial, final):
       return board
   
 
+#actual code VVVV
+
+
+
+      
+#creates/prints the board
 board = createBoard()
-for row in board:
-    print(row)
 
-placeX = int(input("input X coordinate of piece that you would like to move: "))
-placeY = int(input("input Y coordinate tof piece that you would like to move: "))
+#grabs players name and randomly gives them dark or light pieces
+NameSelection()
 
-movingX = int(input("input X coordinate that you would like to move to: "))
-movingY = int(input("input Y coordinate that you would like to move to: "))
+#laying out grid of chess board important for player to know when they need to move
+print("IMPORTANT")
+print("keep in mind these are points of board:")
+print(" + 0  |  1  |  2  |  3  |  4  |  5  |  6  |  7 \n 0 \n - \n 1\n -  \n 2\n -  \n 3\n -  \n 4\n -  \n 5\n -  \n 6\n -  \n 7 \n ")
 
-if board[placeY][placeX]== "♟":
-  board = movePawn(board, "Black", (placeY, placeX), (movingY, movingX))
 
-if board[placeY][placeX]== "♞":
-  board = moveKnight(board, "Black", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♝":
-  board = moveBishop(board, "Black", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♜":
-  board = moveRook(board, "Black", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♚":
-  board = moveQueen(board, "Black", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♛":
-  board = moveKing(board, "Black", (placeY, placeX), (movingY, movingX))
-
-for row in board:
-    print(row)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+while True:
+  #win check for light
+  a = 8
+  y = 8
+  for i in range (len(board)):
+    if "♛" not in board[i]:
+      a = a - 1
+      if a == 0:
+        print("game over")
+        print("light wins")
+        break
+      else:
+        None
+    else:
+        None
   
-placeX = int(input("input X coordinate of piece that you would like to move: "))
-placeY = int(input("input Y coordinate tof piece that you would like to move: "))
-
-movingX = int(input("input X coordinate that you would like to move to: "))
-movingY = int(input("input Y coordinate that you would like to move to: "))
-
-if board[placeY][placeX]== "♙":
-  board = movePawn(board, "White", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♘":
-  board = moveKnight(board, "White", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♗":
-  board = moveBishop(board, "White", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♖":
-  board = moveRook(board, "White", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♔":
-  board = moveQueen(board, "White", (placeY, placeX), (movingY, movingX))
-
-if board[placeY][placeX]== "♕":
-  board = moveKing(board, "White", (placeY, placeX), (movingY, movingX))
-
-
-for row in board:
+  print("Its " + darkPlayer[0] + " turn to move(DARK)")
+  
+  for row in board:
+    print(row) 
+    
+  #asks you the X and y coordinate of what piece you wanna move
+  placeX = int(input("input X coordinate of piece that you would like to move: "))
+  placeY = int(input("input Y coordinate tof piece that you would like to move: "))
+    
+  #asks you where you want to move that piece
+  movingX = int(input("input X coordinate that you would like to move to: "))
+  movingY = int(input("input Y coordinate that you would like to move to: "))
+    
+    
+  #since dark moves first these if statements will check that you're moving a dark piece
+  if board[placeY][placeX]== "♟":
+    board = movePawn(board, "Black", (placeY, placeX), (movingY, movingX))
+      
+  if board[placeY][placeX]== "♞":
+    board = moveKnight(board, "Black", (placeY, placeX), (movingY, movingX))
+      
+  if board[placeY][placeX]== "♝":
+    board = moveBishop(board, "Black", (placeY, placeX), (movingY, movingX))
+      
+  if board[placeY][placeX]== "♜":
+    board = moveRook(board, "Black", (placeY, placeX), (movingY, movingX))
+      
+  if board[placeY][placeX]== "♚":
+    board = moveQueen(board, "Black", (placeY, placeX), (movingY, movingX))
+      
+  if board[placeY][placeX]== "♛":
+    board = moveKing(board, "Black", (placeY, placeX), (movingY, movingX))
+      
+  for row in board:
     print(row)
+       
+  #~~~~~~~~~~Light side~~~~~~~~~~~~~~~~~~~~~
 
 
+  #win check for dark
+  for i in range (len(board)):
+    if "♕" not in board[i]:
+      y = y - 1
+      if y == 0:
+        print("game over")
+        print("dark wins")
+        break
+      else:
+        None
+    else:
+      None
 
-
-
-
-
-
-
-
-
-
-
-
-
-#save deleted code if needed here VVVVVV
-'''
-[":chess_pawn:",":chess_pawn:",":chess_pawn:",":chess_pawn:",":chess_pawn:",":chess_pawn:",":chess_pawn:",":chess_pawn:"]
-
-
-
-'''
+  print("Its " + lightPlayer[0] + " turn to move(LIGHT)")
+  
+  placeX = int(input("input X coordinate of piece that you would like to move: "))
+  placeY = int(input("input Y coordinate tof piece that you would like to move: "))
+  
+  movingX = int(input("input X coordinate that you would like to move to: "))
+  movingY = int(input("input Y coordinate that you would like to move to: "))
+  
+  
+  #since light moves second these if statements will check that you're moving a light piece
+  
+  if board[placeY][placeX]== "♙":
+    board = movePawn(board, "White", (placeY, placeX), (movingY, movingX))
+  
+  if board[placeY][placeX]== "♘":
+    board = moveKnight(board, "White", (placeY, placeX), (movingY, movingX))
+  
+  if board[placeY][placeX]== "♗":
+    board = moveBishop(board, "White", (placeY, placeX), (movingY, movingX))
+  
+  if board[placeY][placeX]== "♖":
+    board = moveRook(board, "White", (placeY, placeX), (movingY, movingX))
+  
+  if board[placeY][placeX]== "♔":
+    board = moveQueen(board, "White", (placeY, placeX), (movingY, movingX))
+  
+  if board[placeY][placeX]== "♕":
+    board = moveKing(board, "White", (placeY, placeX), (movingY, movingX))
+  
+  
+  for row in board:
+      print(row)
+  
+  
+  
